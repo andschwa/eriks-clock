63660e38b182e9d3e226f142f0d250563fdfb2a8
Andrew Schwartzmeyer <andrew@schwartzmeyer.com>
2013-10-31 12:49:56 -0700

    Adding basic clock source file

---
+++ src/clock.ino
@@ -0,0 +1,18 @@
+/*
+  Basic code to display a desired number on an Adafruit 7 segment
+  display.
+
+  File: clock.ino
+  Author: Andrew Schwartzmeyer
+ */
+
+#include <Wire.h>
+
+#include "Adafruit_LEDBackpack.h"
+#include "Adafruit_GFX.h"
+
+Adafruit_7segment matrix = Adafruit_7segment();
+
+void setup() {}
+
+void loop() {}
