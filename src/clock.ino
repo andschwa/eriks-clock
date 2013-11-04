e79a717bdb6d40a96b42047cb8dd209a43d3acf1
Andrew Schwartzmeyer <andrew@schwartzmeyer.com>
2013-11-03 23:45:19 -0800

    Adding interrupts notes, fixing attachment, renaming ISR

--- src/clock.ino
+++ src/clock.ino
@@ -9,6 +9,12 @@
   UNO Analog #5, #4
   Leonardo Digital #3, #2
   Mega Digital #21, #20
+
+  Interrupts 0, 1
+  Uno Digital #2, #3
+  Leonardo Digital #3, #2
+  Mega Digital #2, #3, #21, #20, #19, #18
+  attachInterrupt(interrupt, function, mode) i.e. not the pin number
  */
 
 // Includes
@@ -25,15 +31,13 @@ const unsigned long reset_time = 86400000;
 
 // Constants
 
+const int interrupt = 0;
+
 const int wait = 1000;
 const int serial_rate = 9600;
 
 const uint8_t brightness = 12;
 const int display_address = 0x77;
-
-const int interrupt_pin = 2;
-volatile int pin_state = LOW;
-volatile int state = LOW;
 
 const int millis_in_minute = 60000;
 
@@ -48,7 +52,7 @@ double avg = 0.0;
 // Prototypes
 void display_setup();
 void display_double(double num);
-void pendulum_interrupt_call();
+void swing_ISR();
 
 // Main code
 
@@ -71,7 +75,7 @@ void display_double(double num) {
   matrix.writeDisplay();
 }
 
-void pendulum_interrupt_call() {
+void swing_ISR() {
   new_time = millis();
   period = (new_time - old_time) / 2;
   old_time = new_time;
@@ -84,7 +88,7 @@ void setup() {
   Serial.print("Setup\n");
   display_setup();
   Serial.print("Past display setup\n");
-  attachInterrupt(interrupt_pin, pendulum_interrupt_call, FALLING);
+  attachInterrupt(interrupt, swing_ISR, FALLING);
   Serial.print("Interrupt attached\n");
 }
 
