22671b884b0d0e42bf4c639263e26348ad533695
Andrew Schwartzmeyer <andrew@schwartzmeyer.com>
2013-12-04 19:23:09 -0800

    Adding LED strip; bringing code up to standards

--- src/clock.ino
+++ src/clock.ino
@@ -18,38 +18,48 @@
 
  */
 
-// Includes
+/* Defines */
+#define SERIAL
+
+/* Includes */
 #include <Wire.h>
 
 #include "Adafruit_LEDBackpack.h"
 #include "Adafruit_GFX.h"
+#include "Adafruit_NeoPixel.h"
 
+/* Constants */
+const int INTERRUPT = 0;
+
+const int SAMPLE_SIZE = 60;
+
+const uint8_t BRIGHTNESS = 15;
+const int DISPLAY_ADDRESS = 0x70;  /* I2C */
+const int PRINT_DELAY = 2000;
+
+/* Pins */
+const int LEDSTRIP_PIN = 6;  /* digital */
+
+/* LED strip */
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(60, LEDSTRIP_PIN, NEO_GRB + NEO_KHZ800);
+
+/* 7-seg display */
 Adafruit_7segment matrix = Adafruit_7segment();
 
-// Constants
+/* Globals */
 
-const int interrupt = 0;
-
-const int sample_size = 72;
-
-const int wait = 1200;
-const int serial_rate = 9600;
-
-const uint8_t brightness = 15;
-const int display_address = 0x70;
-
-// Globals :(
+unsigned long print_time = 0;
 
 volatile unsigned long prior_time = 0;
 
 volatile unsigned long sample = 0;
-volatile unsigned long sample_times[sample_size];
+volatile unsigned long sample_times[SAMPLE_SIZE];
 volatile int sample_index = 0;
 
 volatile unsigned long cumulative_average = 0;
 volatile unsigned long cumulative_index = 0;
 
-// Prototypes
+/* Prototypes */
 
 void display_setup();
 void display_double(double num);
@@ -57,18 +67,17 @@ void swing_ISR();
 unsigned long get_sample_total();
 unsigned long get_rolling_average();
 
-// Main code
+/* Main code */
 
 void display_setup() {
-  /* matrix.setBrightness(brightness); */
-  /* Serial.print("Past brightness\n"); */
-  matrix.begin(display_address);
+  matrix.setBrightness(BRIGHTNESS);
+  matrix.begin(DISPLAY_ADDRESS);
 }
 
 void display_double(double num) {
-  int expanded = int(100*num);  // Get first four digits into an int
+  int expanded = int(100 * num);  /* Get first four digits into an int */
   matrix.writeDigitNum(0, (expanded / 1000) % 10, false);
-  matrix.writeDigitNum(1, (expanded / 100) % 10, true);  // Draw dot
+  matrix.writeDigitNum(1, (expanded / 100) % 10, true);  /* Draw dot */
   matrix.drawColon(false);
   matrix.writeDigitNum(3, (expanded / 10) % 10, false);
   matrix.writeDigitNum(4, expanded % 10, false);
@@ -77,43 +86,62 @@ void display_double(double num) {
 
 void swing_ISR() {
   sample = millis() - prior_time;
-  prior_time = millis();  // millis doesn't change in an ISR
-  // millis will, however, overflow and reset to 0 in 49.7103 days
+#ifdef SERIAL
+  Serial.println(sample);
+#endif
+  if (sample < 500) return;
+  prior_time = millis();  /* millis doesn't change in an ISR */
+  /* millis will, however, overflow and reset to 0 in 49.7103 days */
 
   sample_times[sample_index] = sample;
-  sample_index = (sample_index + 1) % sample_size;
+  sample_index = (sample_index + 1) % SAMPLE_SIZE;
 
   cumulative_average = (((cumulative_index * cumulative_average) + sample) /
 			cumulative_index + 1);
   cumulative_index += 1;
+  return;
 }
 
 unsigned long get_sample_total() {
   unsigned long total = 0;
-  for (int i = 0; i < sample_size; i++) {
-    total += sample_times[i];
-  }
+  for (int i = 0; i < SAMPLE_SIZE; i++) total += sample_times[i];
   return total;
 }
 
 unsigned long get_rolling_average() {
-  return (get_sample_total() / ((cumulative_index < sample_size)
-			       ? cumulative_index : sample_size));
+  return (get_sample_total() / ((cumulative_index < SAMPLE_SIZE)
+			       ? cumulative_index : SAMPLE_SIZE));
+}
+
+void colorWipe(uint32_t c, uint8_t wait) {
+  for(uint16_t i = 0; i < strip.numPixels(); i++) {
+      strip.setPixelColor(i, c);
+      strip.show();
+      delay(wait);
+  }
 }
 
 void setup() {
+#ifdef SERIAL
+  Serial.begin(9600);
+#endif
   display_setup();
 
-  attachInterrupt(interrupt, swing_ISR, FALLING);
+  attachInterrupt(INTERRUPT, swing_ISR, FALLING);
 
-  for (int i = 0; i < sample_size; i++) {
-    sample_times[i] = 0;
-  }
+  strip.begin();
+  colorWipe(strip.Color(255, 255, 255), 60);
 
-  delay(wait);
+  for (int i = 0; i < SAMPLE_SIZE; i++) sample_times[i] = 0;
+
+  delay(1200);
+  print_time = millis();
 }
 
 void loop() {
-  double bpm = 60000 / double(get_rolling_average());
-  display_double(2 * bpm);
+  if (millis() > print_time) {
+    double bpm = 60000 / (double)get_rolling_average();
+    print_time = millis() + PRINT_DELAY;
+    display_double(bpm);
+  }
 }
